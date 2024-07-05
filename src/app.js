f1e3da08748d7a57a020787ae857962deeed7b59
Vikash <vikashsinghal509@gmail.com>
2024-07-05 08:46:10 +0530

    server and mongo connected

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require("./db/conn");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public")
+
+app.use(express.static(static_path));
+
+app.get('/', (req, res) => {
+    res.send("This my life");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
